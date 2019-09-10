f42ead4d671d8936b1b434767e8a3ca5ef891005
Chris <cjmarsh725@gmail.com>
2019-09-10 13:51:10 -0700

    Added bcrypt to user model

--- users/User.js
+++ users/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const SALT_ROUNDS = 11;
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -11,6 +14,18 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true
   }
-})
+});
+
+userSchema.pre('save', function(next) {
+  bcrypt.hash(this.password, SALT_ROUNDS, (err, hash) => {
+    if (err) return next(err);
+    this.password = hash;
+    return next();
+  })
+});
+
+userSchema.methods.checkPassword = function(plainTextPW, callBack) {
+  bcrypt.compare(plainTextPW, this.password, callBack);
+};
 
 module.exports = mongoose.model('User', userSchema);
