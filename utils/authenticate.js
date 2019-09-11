e92ea411bdddfdbf5e453bb0102ff833fa3a81c9
Chris <cjmarsh725@gmail.com>
2019-09-11 11:29:47 -0700

    Authentication middleware

---
+++ utils/authenticate.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
+const authenticate = (req, res, next) => {
+  const token = req.get('Authorization');
+  if (token) {
+    jwt.verify(token, process.env.SECRET, (err, decoded) => {
+      if (err) return res.status(422).json(err);
+      req.decoded = decoded;
+      next();
+    });
+  } else {
+    return res.status(403).json({
+      error: 'No token provided, must be set on the Authorization Header'
+    });
+  }
+};
+
+module.exports = {
+  authenticate
+};
