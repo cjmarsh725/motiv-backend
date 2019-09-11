c5abaadffc72bf09d99d6b4908a7880f2a9ba9eb
Chris <cjmarsh725@gmail.com>
2019-09-11 11:20:20 -0700

    Created signin route with jwt

--- users/userRoutes.js
+++ users/userRoutes.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
 const User = require('./User');
 
 router.post('/signup', (req, res) => {
@@ -9,6 +11,34 @@ router.post('/signup', (req, res) => {
   user.save()
     .then(user => res.status(201).json(user))
     .catch(err => res.status(500).json(err));
+})
+
+router.post('/signin', (req, res) => {
+  const { username, password } = req.body;
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      res.status(403).json({ error: 'Invalid Username/Password'});
+      return;
+    }
+    if (user === null) {
+      res.status(422).json({ error: 'No user with that username in the database'});
+      return;
+    }
+    user.checkPassword(password, (nonMatch, hashMatch) => {
+      if (nonMatch !== undefined) {
+        res.status(422).json({ error: 'Passwords dont match'});
+        return;
+      }
+      if (hashMatch) {
+        const payload = {
+          username: user.username,
+          id: user._id
+        }
+        const token = jwt.sign(payload, process.env.SECRET);
+        res.json({ token });
+      }
+    })
+  })
 })
 
 module.exports = router;
