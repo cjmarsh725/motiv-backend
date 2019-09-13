8a4e7289459f02e2ecd0ed0cf0c6b9eb68e1ea54
Chris <cjmarsh725@gmail.com>
2019-09-13 12:53:20 -0700

    Route for adding a new reminder

--- reminders/reminderRoutes.js
+++ reminders/reminderRoutes.js
@@ -13,4 +13,18 @@ router.get('/', authenticate, (req, res) => {
   }
 })
 
+router.post('/add', authenticate, (req, res) => {
+  if (req.decoded) {
+    const { content } = req.body;
+    const { id } = req.decoded;
+    const reminderData = { content: content, userid: id };
+    const reminder = new Reminder(reminderData);
+    reminder.save()
+      .then(reminder => res.status(201).json(reminder))
+      .catch(err => res.status(500).json(err));
+  } else {
+    res.status(422).json({ msg: 'Invalid authorization'});
+  }
+})
+
 module.exports = router;
