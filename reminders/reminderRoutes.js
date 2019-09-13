89dc40a8d122ad4f46e39a2033898aae5550f19b
Chris <cjmarsh725@gmail.com>
2019-09-13 12:45:44 -0700

    Route for getting all reminders of a user

--- reminders/reminderRoutes.js
+++ reminders/reminderRoutes.js
@@ -3,9 +3,14 @@ const router = express.Router();
 const { authenticate } = require('../utils/authenticate');
 const Reminder = require('./Reminder');
 
-router.get('/', authenticate, (req, res) => {
-  console.log(req.decoded);
-  res.json({ msg: 'this is a test' });
+router.get('/', authenticate, (req, res) => { 
+  if (req.decoded) {
+    Reminder.find({ userid: req.decoded.id })
+      .then(reminders => res.json(reminders))
+      .catch(err => res.status(500).json(err));
+  } else {
+    res.status(422).json({ msg: 'Invalid authorization'});
+  }
 })
 
 module.exports = router;
