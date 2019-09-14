d8a23de3e59b7d9fe2b0847872ebab4e0113079d
Chris <cjmarsh725@gmail.com>
2019-09-14 16:03:11 -0700

    Implemented move route to swap reminders id

--- reminders/reminderRoutes.js
+++ reminders/reminderRoutes.js
@@ -18,15 +18,32 @@ router.post('/add', authenticate, (req, res) => {
     const { content } = req.body;
     const { id } = req.decoded;
     Reminder.find({ userid: id }, null, { sort: { order: 'desc' }}).then(reminders => {
-      const id = reminders.length === 0 ? 0 : reminders[0].id + 1;
-      const reminderData = { content: content, id: id, userid: id };
+      const reminderid = reminders.length === 0 ? 0 : reminders[0].id + 1;
+      const reminderData = { content: content, id: reminderid, userid: id };
       const reminder = new Reminder(reminderData);
       reminder.save()
         .then(reminder => res.status(201).json(reminder))
         .catch(err => res.status(500).json(err));
     }).catch(err => res.status(500).json(err));
   } else {
-    res.status(422).json({ msg: 'Invalid authorization'});
+    res.status(422).json({ msg: 'Invalid authorization' });
+  }
+})
+
+router.post('/move', authenticate, (req, res) => {
+  if (req.decoded) {
+    const { movedFrom, movedTo } = req.body;
+    const { id } = req.decoded;
+    Reminder.find({ userid: id, $or: [{id: movedTo},{id: movedFrom}] }).then(reminders => {
+      const temp = reminders[0].content;
+      reminders[0].content = reminders[1].content;
+      reminders[1].content = temp;
+      reminders[0].save();
+      reminders[1].save();
+      res.json({ msg: 'Move successfully completed' });
+    }).catch(err => res.status(500).json(err));
+  } else {
+    res.status(422).json({ msg: 'Invalid authorization' });
   }
 })
 
