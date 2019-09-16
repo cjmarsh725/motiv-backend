f13c64781e4b36e584613cb063f4531ef3a1da43
Chris <cjmarsh725@gmail.com>
2019-09-16 11:21:11 -0700

    Added moment package and sort by date

--- appointments/appointmentRoutes.js
+++ appointments/appointmentRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const moment = require('moment');
 const { authenticate } = require('../utils/authenticate');
 const Appointment = require('./Appointment');
 
@@ -7,6 +8,11 @@ router.get('/', authenticate, (req, res) => {
   if (req.decoded) {
     Appointment.find({ userid: req.decoded.userid })
       .then(appointments => {
+        appointments.sort((a, b) => {
+          const m1 = moment(a.date);
+          const m2 = moment(b.date);
+          return m1 - m2;
+        })
         res.json(appointments)
       })
       .catch(err => res.status(500).json(err));
