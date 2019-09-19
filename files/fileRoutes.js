4334f7af28e78e0fa6190a64b13eccf71f017897
Chris <cjmarsh725@gmail.com>
2019-09-19 12:18:16 -0700

    Add route implemented

--- files/fileRoutes.js
+++ files/fileRoutes.js
@@ -13,4 +13,17 @@ router.get('/', authenticate, (req, res) => {
   }
 })
 
+router.post('/add', authenticate, (req, res) => {
+  if (req.decoded) {
+    const { fileStructure } = req.body;
+    const { userid } = req.decoded;
+    const file = new File({ userid, fileStructure });
+    file.save()
+      .then(file => res.status(201).json(file))
+      .catch(err => res.status(500).json(err));
+  } else {
+    res.status(422).json({ msg: 'Invalid authorization' });
+  }
+})
+
 module.exports = router;
