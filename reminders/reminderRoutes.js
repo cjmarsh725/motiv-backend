4bc094d4a3c0f2891a19655296604bcab2e10a97
Chris <cjmarsh725@gmail.com>
2019-09-21 12:59:47 -0700

    Improved commenting

--- reminders/reminderRoutes.js
+++ reminders/reminderRoutes.js
@@ -3,8 +3,17 @@ const router = express.Router();
 const { authenticate } = require('../utils/authenticate');
 const Reminder = require('./Reminder');
 
+/*
+This is the sub-router that handles the routes responsible for dealing with reminders,
+including retrieving them all, adding a new one, switching the content of two reminders,
+and deleting one or all of them.
+*/
+
+// Route responsible for retrieving all the reminders of a user
 router.get('/', authenticate, (req, res) => { 
+  // The decoded property exists if the jwt was successfully parsed from the auth header
   if (req.decoded) {
+    // Finds all documents with the given userid and returns them in the response
     Reminder.find({ userid: req.decoded.userid })
       .then(reminders => res.json(reminders))
       .catch(err => res.status(500).json(err));
@@ -13,14 +22,19 @@ router.get('/', authenticate, (req, res) => {
   }
 })
 
+// Route responsible for adding a new reminder for a user
 router.post('/add', authenticate, (req, res) => {
+  // The decoded property exists if the jwt was successfully parsed from the auth header
   if (req.decoded) {
     const { content } = req.body;
     const { userid } = req.decoded;
-    Reminder.find({ userid: userid }, null, { sort: { order: 'desc' }}).then(reminders => {
-      const reminderid = reminders.length === 0 ? 0 : reminders[0].id + 1;
-      const reminderData = { content: content, id: reminderid, userid: userid };
+    // All the reminders for the user are sorted by order to get the next highest order value
+    Reminder.find({ userid }, null, { sort: { order: 'desc' }}).then(reminders => {
+      // Sets reminderid to 0 or one higher than the previously highest order value
+      const id = reminders.length === 0 ? 0 : reminders[0].id + 1;
+      const reminderData = { content, id, userid };
       const reminder = new Reminder(reminderData);
+      // Saves the new reminder to the db
       reminder.save()
         .then(reminder => res.status(201).json(reminder))
         .catch(err => res.status(500).json(err));
@@ -30,14 +44,20 @@ router.post('/add', authenticate, (req, res) => {
   }
 })
 
+// Route responsible for swapping the content of two reminders to simulate moving them
 router.post('/move', authenticate, (req, res) => {
+  // The decoded property exists if the jwt was successfully parsed from the auth header
   if (req.decoded) {
+    // Body includes two ids to swap
     const { movedFrom, movedTo } = req.body;
     const { userid } = req.decoded;
+    // Gets the two reminders based on the given ids
     Reminder.find({ userid: userid, $or: [{id: movedTo},{id: movedFrom}] }).then(reminders => {
+      // Swaps the content of the reminders with a temp variable
       const temp = reminders[0].content;
       reminders[0].content = reminders[1].content;
       reminders[1].content = temp;
+      // Saves both reminders to the db
       reminders[0].save();
       reminders[1].save();
       res.json({ msg: 'Move successfully completed' });
@@ -47,10 +67,13 @@ router.post('/move', authenticate, (req, res) => {
   }
 })
 
+// Route responsible for deleting a single reminder for a user
 router.post('/delete', authenticate, (req, res) => {
+  // The decoded property exists if the jwt was successfully parsed from the auth header
   if (req.decoded) {
     const { id } = req.body;
     const { userid } = req.decoded;
+    // Finds and deletes the reminder with the given userid and id
     Reminder.findOneAndDelete({ userid, id }).then(reminder => {
       if (reminder) res.json({ msg: 'Reminder deleted' });
       else res.status(422).json({ msg: 'Invalid id'});
@@ -60,9 +83,12 @@ router.post('/delete', authenticate, (req, res) => {
   }
 })
 
+// Route responsible for deleting all of the reminders for a user
 router.post('/deleteall', authenticate, (req, res) => {
+  // The decoded property exists if the jwt was successfully parsed from the auth header
   if (req.decoded) {
     const { userid } = req.decoded;
+    // Deletes all reminders with the given userid
     Reminder.deleteMany({ userid })
       .then(reminders => res.json({ msg: 'All reminders deleted' }))
       .catch(err => res.status(500).json(err));
