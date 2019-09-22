d7d1293b096404690925fe3eefb4bb7a204e3129
Chris <cjmarsh725@gmail.com>
2019-09-22 12:55:02 -0700

    Improved commenting

--- files/fileRoutes.js
+++ files/fileRoutes.js
@@ -3,9 +3,19 @@ const router = express.Router();
 const { authenticate } = require('../utils/authenticate');
 const File = require('./File');
 
+/*
+This is the sub-router that handles the routes responsible for dealing with files,
+including retrieving the file for a given user, adding a new one, updating a file,
+and deleting one.
+*/
+
+// Route responsible for retrieving the unique file for the provided userid
 router.get('/', authenticate, (req, res) => { 
+  // The decoded property exists if the jwt was successfully parsed from the auth header
   if (req.decoded) {
-    File.findOne({ userid: req.decoded.userid })
+    const { userid } = req.decoded;
+    // Gets the unique file for the userid and returns it
+    File.findOne({ userid })
       .then(file => res.json(file))
       .catch(err => res.status(500).json(err));
   } else {
@@ -13,10 +23,13 @@ router.get('/', authenticate, (req, res) => {
   }
 })
 
+// Route responsible for adding a new file for a user if it doesn't exist yet
 router.post('/add', authenticate, (req, res) => {
+  // The decoded property exists if the jwt was successfully parsed from the auth header
   if (req.decoded) {
     const { fileStructure } = req.body;
     const { userid } = req.decoded;
+    // Creates a new file based on the provided data and saves it to the db
     const file = new File({ userid, fileStructure });
     file.save()
       .then(file => res.status(201).json(file))
@@ -26,10 +39,13 @@ router.post('/add', authenticate, (req, res) => {
   }
 })
 
+// Route responsible for updating the file of a given user
 router.post('/update', authenticate, (req, res) => {
+  // The decoded property exists if the jwt was successfully parsed from the auth header
   if (req.decoded) {
     const { fileStructure } = req.body;
     const { userid } = req.decoded;
+    // Updates the unique file of a user with the provided fileStructure
     File.findOneAndUpdate({ userid }, { fileStructure })
       .then(file => res.json({ msg: 'File structure updated' }))
       .catch(err => res.status(500).json(err));
@@ -38,7 +54,9 @@ router.post('/update', authenticate, (req, res) => {
   }
 })
 
+// Route responsible for deleting the unique file of a user
 router.post('/delete', authenticate, (req, res) => {
+  // The decoded property exists if the jwt was successfully parsed from the auth header
   if (req.decoded) {
     const { userid } = req.decoded;
     File.findOneAndDelete({ userid })
