2ceee7a370226e9f9a3a05ffb6cf98223cc96dd4
Chris <cjmarsh725@gmail.com>
2019-09-22 12:48:29 -0700

    Improved commenting

--- appointments/appointmentRoutes.js
+++ appointments/appointmentRoutes.js
@@ -4,10 +4,18 @@ const moment = require('moment');
 const { authenticate } = require('../utils/authenticate');
 const Appointment = require('./Appointment');
 
+/*
+This is the sub-router that handles the routes responsible for dealing with appointments,
+including retrieving them all sorted by date, adding a new one, and deleting one or all of them.
+*/
+
+// Route responsible for retrieving all the appointments of a user, sorted by date
 router.get('/', authenticate, (req, res) => { 
+  // The decoded property exists if the jwt was successfully parsed from the auth header
   if (req.decoded) {
     Appointment.find({ userid: req.decoded.userid })
       .then(appointments => {
+        // Sorts the array by the date property using moment
         appointments.sort((a, b) => {
           const m1 = moment(a.date);
           const m2 = moment(b.date);
@@ -21,19 +29,25 @@ router.get('/', authenticate, (req, res) => {
   }
 })
 
+// Route responsible for adding a new appointment for a user
 router.post('/add', authenticate, (req, res) => {
+  // The decoded property exists if the jwt was successfully parsed from the auth header
   if (req.decoded) {
     const { label, date } = req.body;
     const { userid } = req.decoded;
+    // Ensures that both label and date fields were included in the request body
     if (!label || !date) {
       res.status(422).json({ msg: 'Must include label and date fields' });
       return;
     }
+    // Regex to partially match date and ensure it includes a time
     const regex = /[0-9]+-[0-9]+-[0-9]+\s[0-9]+:[0-9]+\s[AP]M/;
+    // Check to see if the date satisfies the regex and is a valid moment date
     if (!regex.test(date) || !moment(date, 'MM-DD-YYYY h:mm A').isValid()) {
       res.status(422).json({ msg: 'Invalid date' });
       return;
     }
+    // Creates a new appointment and saves it to the db
     const appointmentData = { label, date, userid };
     const appointment = new Appointment(appointmentData);
     appointment.save()
@@ -44,14 +58,18 @@ router.post('/add', authenticate, (req, res) => {
   }
 })
 
+// Route responsible for deleting appointments based on the array of ids passed in the request
 router.post('/delete', authenticate, (req, res) => {
+  // The decoded property exists if the jwt was successfully parsed from the auth header
   if (req.decoded) {
     const { userid } = req.decoded;
     const { ids } = req.body;
+    // Ensures the ids property exists and is an array
     if (!Array.isArray(ids)) {
       res.status(422).json({ msg: 'Must include array of ids to be deleted' });
       return;
     }
+    // Deletes appointments based on ids for the user
     Appointment.deleteMany({ userid, _id: { $in: ids }})
       .then(appointments => res.json({ msg: ids.length + ' appointments deleted' }))
       .catch(err => res.status(500).json(err));
@@ -60,9 +78,12 @@ router.post('/delete', authenticate, (req, res) => {
   }
 })
 
+// Route responsible for deleting all the appointments for a given user
 router.post('/deleteall', authenticate, (req, res) => {
+  // The decoded property exists if the jwt was successfully parsed from the auth header
   if (req.decoded) {
     const { userid } = req.decoded;
+    // Deletes all appointments for the user
     Appointment.deleteMany({ userid })
       .then(appointments => res.json({ msg: 'All appointments deleted' }))
       .catch(err => res.status(500).json(err));
