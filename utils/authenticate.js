0d548f488cd9c88d659655a6279ed77b79b9e54c
Chris <cjmarsh725@gmail.com>
2019-09-22 17:44:10 -0700

    Improved commenting

--- utils/authenticate.js
+++ utils/authenticate.js
@@ -1,15 +1,24 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
+/*
+Middleware that handles the json web token decoding. The jwt's payload is used to store
+a userid that is in turn used to retrieve relevant documents from the database. This
+auth middleware is used in the routes that require the userid to operate.
+*/
+
 const authenticate = (req, res, next) => {
+  // In a request the token is placed in the header labeled Authorization
   const token = req.get('Authorization');
   if (token) {
+    // Checks to verify the token is legitimate using the secret
     jwt.verify(token, process.env.SECRET, (err, decoded) => {
       if (err) return res.status(422).json(err);
       req.decoded = decoded;
       next();
     });
   } else {
+    // Routes that use this middleware require the token to be present
     return res.status(403).json({
       error: 'No token provided, must be set on the Authorization Header'
     });
